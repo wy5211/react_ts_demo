943318054d05ec85b1ca532ae7f340c7779e0e98
王阳 <wangyang@jiaogepengyou.com.cn>
2022-05-15 14:12:54 +0800

    chore: add lint for project

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
 module.exports = {
-  extends: [require.resolve('@umijs/fabric/dist/eslint')],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    commonjs: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {},
 };
--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,6 @@ import React from 'react';
 function App() {
   console.log('123');
 
-  console.log(a.a);
-
   return <div className="App">app</div>;
 }
 
